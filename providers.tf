6d96e51fecccf0d16fb9defd33dcd897989bcadd
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2024-03-24 20:58:32 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+#Statefile remote backup using s3 bucket and locking with dynamodb
+terraform {
+  backend "s3" {
+    bucket         = "insureme-project1"
+    key            = "terraform.tfstate.d/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "insuremestatefile1"
+  }
+}
+
