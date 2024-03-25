2ea47f17ab3428550dc8a768032cf7cd4b70c5e6
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2024-03-25 12:27:34 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -19,5 +19,10 @@ terraform {
     region         = "ap-south-1"
     dynamodb_table = "insuremestatefile1"
   }
+}provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
+
 
