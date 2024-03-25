fca2e1dd0d6c5c0460bb149a2fbc66a8eb5dd8a5
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2024-03-25 17:37:14 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,9 +7,8 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region = var.region
 }
  
